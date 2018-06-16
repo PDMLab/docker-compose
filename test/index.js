88894128400301fd80fb8d0e1e28ccfded52ed10
Ignatiev Mikhail <ignatiev.reg@gmail.com>
2018-06-16 13:00:24 +0300

    custom ymls support + tests, eslint aid for tests

--- test/index.js
+++ test/index.js
@@ -3,71 +3,74 @@
 const compose = require('../index');
 const path = require('path');
 const tape = require('tape');
-const _test = require('tape-promise').default;
-const test = _test(tape);
+const defaultTest = require('tape-promise').default;
+const test = defaultTest(tape);
 const Docker = require('dockerode');
 const docker = new Docker();
+
+const isContainerRunning = async name => new Promise((resolve, reject) => {
+  docker.listContainers((err, containers) => {
+    if (err) {
+      reject(err);
+    }
+
+    const running = containers.find(container => container.Names.includes(name));
+
+    resolve(running);
+  });
+});
 
 test('ensure container gets started', async assert => {
   await compose.up({ cwd: path.join(__dirname), log: true });
 
-  docker.listContainers((err, containers) => {
-    const containerIsRunning = containers.find(container =>
-      container.Names.includes('/compose_test_mongodb')
-    );
-
-    assert.true(containerIsRunning);
-    assert.end();
-  });
+  assert.true(await isContainerRunning('/compose_test_mongodb'));
+  assert.end();
 });
 
 test('ensure container gets down', async assert => {
   await compose.up({ cwd: path.join(__dirname), log: true });
   await compose.down({ cwd: path.join(__dirname), log: true });
 
-  docker.listContainers((err, containers) => {
-    // eslint-disable-line
-    const containerIsRunning = containers.find(container =>
-      container.Names.includes('/compose_test_mongodb')
-    );
-
-    assert.false(containerIsRunning);
-    assert.end();
-  });
+  assert.false(await isContainerRunning('/compose_test_mongodb'));
+  assert.end();
 });
 
 test('ensure container gets stopped', async assert => {
   await compose.up({ cwd: path.join(__dirname), log: true });
   await compose.stop({ cwd: path.join(__dirname), log: true });
 
-  docker.listContainers((err, containers) => {
-    // eslint-disable-line
-    const containerIsRunning = containers.find(container =>
-      container.Names.includes('/compose_test_mongodb')
-    );
-
-    assert.false(containerIsRunning);
-    assert.end();
-  });
+  assert.false(await isContainerRunning('/compose_test_mongodb'));
+  assert.end();
 });
 
 test('ensure container gets killed', async assert => {
   await compose.up({ cwd: path.join(__dirname), log: true });
   await compose.kill({ cwd: path.join(__dirname), log: true });
 
-  docker.listContainers((err, containers) => {
-    // eslint-disable-line
-    const containerIsRunning = containers.find(container =>
-      container.Names.includes('/compose_test_mongodb')
-    );
+  assert.false(await isContainerRunning('/compose_test_mongodb'));
+  assert.end();
+});
 
-    assert.false(containerIsRunning);
-    assert.end();
-  });
+test('ensure custom ymls are working', async assert => {
+  const config = './docker-compose-2.yml';
+  const cwd = path.join(__dirname);
+  const log = true;
+
+  await compose.up({ cwd, log, config });
+  assert.true(await isContainerRunning('/compose_test_alpine'));
+
+   // config & [config] is the same thing, ensures that multiple configs are handled properly
+  await compose.kill({ cwd, log, config: [ config ]});
+  assert.false(await isContainerRunning('/compose_test_alpine'));
+  assert.end();
 });
 
 test('teardown', assert => {
   docker.listContainers((err, containers) => {
+    if (err) {
+      throw err;
+    }
+
     containers.forEach(container => {
       container.Names.forEach(name => {
         if (name.startsWith('/compose_test_')) {
