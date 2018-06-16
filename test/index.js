4d7fac4b2a689786d3d591f349119246cabf2987
Ignatiev Mikhail <ignatiev.reg@gmail.com>
2018-06-16 16:42:43 +0300

    exec and run implementation

--- test/index.js
+++ test/index.js
@@ -57,11 +57,43 @@ test('ensure custom ymls are working', async assert => {
   const log = true;
 
   await compose.up({ cwd, log, config });
-  assert.true(await isContainerRunning('/compose_test_alpine'));
+  assert.true(await isContainerRunning('/compose_test_mongodb_2'));
 
-   // config & [config] is the same thing, ensures that multiple configs are handled properly
+   // config & [config] are the same thing, ensures that multiple configs are handled properly
   await compose.kill({ cwd, log, config: [ config ]});
-  assert.false(await isContainerRunning('/compose_test_alpine'));
+  assert.false(await isContainerRunning('/compose_test_mongodb_2'));
+  assert.end();
+});
+
+test('ensure run and exec are working', async assert => {
+  const checkOSID = (out, id) => {
+    // parse /etc/os-release contents
+    const re = /([\w,_]+)=(.*)/g;
+    let match = null;
+    const os = {};
+
+    while ((match = re.exec(out)) !== null) { // eslint-disable-line no-cond-assign
+      os[match[1]] = match[2];
+    }
+
+    assert.equals(os.ID, id);
+  };
+
+  const opts = { cwd: path.join(__dirname), log: false };
+
+  await compose.up(opts);
+
+  assert.true(await isContainerRunning('/compose_test_mongodb'));
+
+  let std = await compose.exec('db', 'cat /etc/os-release', opts);
+
+  assert.false(std.err);
+  checkOSID(std.out, 'debian');
+
+  std = await compose.run('alpine', 'cat /etc/os-release', opts);
+  assert.false(std.err);
+  checkOSID(std.out, 'alpine');
+
   assert.end();
 });
 
