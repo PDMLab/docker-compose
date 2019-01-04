ae9786d4a437e5782d35623f8950154ae47fe398
MartinJLee <therhymeorange@gmail.com>
2019-01-04 10:37:59 +1000

    update Typescript typings

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
 declare module "docker-compose" {
   interface IDockerCompose {
     upAll(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
-    upMany(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
-    upOne(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
+    upMany(services:String[], options: IDockerComposeOptions): Promise<IDockerComposeResult>;
+    upOne(service:String, options: IDockerComposeOptions): Promise<IDockerComposeResult>;
     kill(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
     down(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
     stop(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
     rm(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
-    exec(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
-    run(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
+    exec(container:String, command:String, options: IDockerComposeOptions): Promise<IDockerComposeResult>;
+    run(service:String, command:String, options: IDockerComposeOptions): Promise<IDockerComposeResult>;
     buildAll(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
-    buildMany(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
-    buildOne(options: IDockerComposeOptions): Promise<IDockerComposeResult>;
+    buildMany(services:String[], options: IDockerComposeOptions): Promise<IDockerComposeResult>;
+    buildOne(service:String, options: IDockerComposeOptions): Promise<IDockerComposeResult>;
   }
 
   interface IDockerComposeOptions {
