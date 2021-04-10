5b1c9fbad36a1d9165c2b3a7fcd8f7dbece27486
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2021-04-10 22:17:43 +0200

    style: use prettier + typescript-eslint

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  semi: false,
+  useTabs: false,
+  jsxBracketSameLine: false,
+  proseWrap: 'never'
+}
