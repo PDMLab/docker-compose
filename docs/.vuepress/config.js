5c14cbe0635dd1fc30bd3885f9b076c5a208b1bf
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2021-04-10 23:44:31 +0200

    docs: setup vuepress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base:'/docker-compose/'
+  // lang: 'en-US',
+  // title: 'Hello, VuePress!',
+  // description: 'This is my first VuePress site',
+
+  // themeConfig: {
+  //   logo: 'https://vuejs.org/images/logo.png'
+  // }
+}
